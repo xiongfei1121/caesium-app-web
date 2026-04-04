164cf50da7ac1fe79abf54960d4e26ffd824aec2
Matteo Paonessa <matteo.paonessa@gmail.com>
2026-04-04 19:01:28 +0200

    Add devcontainer configuration for Caesium Web App and disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@
 import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxtjs/i18n', '@nuxt/eslint', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  ssr: false,
   srcDir: 'app',
   vite: {
     plugins: [tailwindcss()],
